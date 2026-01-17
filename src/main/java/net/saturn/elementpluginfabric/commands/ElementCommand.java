1cb07c398bb1ac2cc5203a5846deb1d5ebe52a6c
Pumpkin <wishsmp1002@gmail.com>
2026-01-17 15:52:39 +1100

    siggma

--- src/main/java/net/saturn/elementpluginfabric/commands/ElementCommand.java
+++ src/main/java/net/saturn/elementpluginfabric/commands/ElementCommand.java
@@ -15,96 +15,95 @@ import net.saturn.elementpluginfabric.elements.ElementType;
 import net.saturn.elementpluginfabric.managers.ElementManager;
 
 public class ElementCommand {
-    
+
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher,
                                 CommandBuildContext registryAccess,
                                 Commands.CommandSelection environment) {
         dispatcher.register(Commands.literal("element")
-            .requires(source -> source.hasPermission(0))
-            .then(Commands.literal("set")
-                .then(Commands.argument("element", StringArgumentType.string())
-                    .suggests((context, builder) -> {
-                        for (ElementType type : ElementType.values()) {
-                            builder.suggest(type.name().toLowerCase());
-                        }
-                        return builder.buildFuture();
-                    })
-                    .executes(ElementCommand::setElement)))
-            .then(Commands.literal("roll")
-                .executes(ElementCommand::rollElement))
-            .then(Commands.literal("reroll")
-                .executes(ElementCommand::rerollElement))
-            .then(Commands.literal("info")
-                .executes(ElementCommand::infoElement)));
+                .requires(source -> source.hasPermission(0))
+                .then(Commands.literal("set")
+                        .then(Commands.argument("element", StringArgumentType.string())
+                                .suggests((context, builder) -> {
+                                    for (ElementType type : ElementType.values()) {
+                                        builder.suggest(type.name().toLowerCase());
+                                    }
+                                    return builder.buildFuture();
+                                })
+                                .executes(ElementCommand::setElement)))
+                .then(Commands.literal("roll")
+                        .executes(ElementCommand::rollElement))
+                .then(Commands.literal("reroll")
+                        .executes(ElementCommand::rerollElement))
+                .then(Commands.literal("info")
+                        .executes(ElementCommand::infoElement)));
     }
-    
+
     private static int setElement(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
         CommandSourceStack source = context.getSource();
-        ServerPlayer player = source.getPlayerOrThrow();
+        ServerPlayer player = source.getPlayerOrException();
         String elementName = StringArgumentType.getString(context, "element");
-        
+
         ElementType type;
         try {
             type = ElementType.valueOf(elementName.toUpperCase());
         } catch (IllegalArgumentException e) {
             source.sendFailure(Component.literal("Invalid element: " + elementName)
-                .withStyle(ChatFormatting.RED));
+                    .withStyle(ChatFormatting.RED));
             return 0;
         }
-        
+
         ElementManager elementManager = ElementPluginFabric.getInstance().getElementManager();
         elementManager.assignElement(player, type);
-        
+
         source.sendSuccess(() -> Component.literal("Element set to: ")
-            .withStyle(ChatFormatting.GREEN)
-            .append(Component.literal(type.name()).withStyle(ChatFormatting.AQUA)), false);
-        
+                .withStyle(ChatFormatting.GREEN)
+                .append(Component.literal(type.name()).withStyle(ChatFormatting.AQUA)), false);
+
         return 1;
     }
-    
+
     private static int rollElement(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
         CommandSourceStack source = context.getSource();
-        ServerPlayer player = source.getPlayerOrThrow();
-        
+        ServerPlayer player = source.getPlayerOrException();
+
         ElementManager elementManager = ElementPluginFabric.getInstance().getElementManager();
         elementManager.rollAndAssign(player);
-        
+
         source.sendSuccess(() -> Component.literal("Rolling for a random element...")
-            .withStyle(ChatFormatting.GOLD), false);
-        
+                .withStyle(ChatFormatting.GOLD), false);
+
         return 1;
     }
-    
+
     private static int rerollElement(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
         CommandSourceStack source = context.getSource();
-        ServerPlayer player = source.getPlayerOrThrow();
-        
+        ServerPlayer player = source.getPlayerOrException();
+
         ElementManager elementManager = ElementPluginFabric.getInstance().getElementManager();
         elementManager.assignRandomDifferentElement(player);
-        
+
         source.sendSuccess(() -> Component.literal("Element rerolled!")
-            .withStyle(ChatFormatting.GOLD), false);
-        
+                .withStyle(ChatFormatting.GOLD), false);
+
         return 1;
     }
-    
+
     private static int infoElement(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
         CommandSourceStack source = context.getSource();
-        ServerPlayer player = source.getPlayerOrThrow();
-        
+        ServerPlayer player = source.getPlayerOrException();
+
         ElementManager elementManager = ElementPluginFabric.getInstance().getElementManager();
         ElementType current = elementManager.getPlayerElement(player);
-        
+
         if (current == null) {
             source.sendSuccess(() -> Component.literal("You don't have an element assigned yet!")
-                .withStyle(ChatFormatting.YELLOW), false);
+                    .withStyle(ChatFormatting.YELLOW), false);
         } else {
             source.sendSuccess(() -> Component.literal("Your current element: ")
-                .withStyle(ChatFormatting.GREEN)
-                .append(Component.literal(current.name()).withStyle(ChatFormatting.AQUA)), false);
+                    .withStyle(ChatFormatting.GREEN)
+                    .append(Component.literal(current.name()).withStyle(ChatFormatting.AQUA)), false);
         }
-        
+
         return 1;
     }
 }
-
--- src/main/java/net/saturn/elementpluginfabric/commands/ManaCommand.java
+++ src/main/java/net/saturn/elementpluginfabric/commands/ManaCommand.java
@@ -15,55 +15,54 @@ import net.saturn.elementpluginfabric.data.PlayerData;
 import net.saturn.elementpluginfabric.managers.ManaManager;
 
 public class ManaCommand {
-    
+
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher,
                                 CommandBuildContext registryAccess,
                                 Commands.CommandSelection environment) {
         dispatcher.register(Commands.literal("mana")
-            .requires(source -> source.hasPermission(0))
-            .executes(ManaCommand::checkMana)
-            .then(Commands.literal("set")
-                .requires(source -> source.hasPermission(2))
-                .then(Commands.argument("amount", IntegerArgumentType.integer(0))
-                    .executes(ManaCommand::setMana))));
+                .requires(source -> source.hasPermission(0))
+                .executes(ManaCommand::checkMana)
+                .then(Commands.literal("set")
+                        .requires(source -> source.hasPermission(2))
+                        .then(Commands.argument("amount", IntegerArgumentType.integer(0))
+                                .executes(ManaCommand::setMana))));
     }
-    
+
     private static int checkMana(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
         CommandSourceStack source = context.getSource();
-        ServerPlayer player = source.getPlayerOrThrow();
-        
+        ServerPlayer player = source.getPlayerOrException();
+
         ManaManager manaManager = ElementPluginFabric.getInstance().getManaManager();
         PlayerData data = ElementPluginFabric.getInstance().getDataStore().getPlayerData(player.getUUID());
-        
+
         int currentMana = data.getMana();
         int maxMana = ElementPluginFabric.getInstance().getConfigManager().getMaxMana();
-        
+
         source.sendSuccess(() -> Component.literal("Ⓜ Mana: ")
-            .withStyle(ChatFormatting.AQUA)
-            .append(Component.literal(String.valueOf(currentMana))
-                .withStyle(ChatFormatting.WHITE))
-            .append(Component.literal("/" + maxMana)
-                .withStyle(ChatFormatting.GRAY)), false);
-        
+                .withStyle(ChatFormatting.AQUA)
+                .append(Component.literal(String.valueOf(currentMana))
+                        .withStyle(ChatFormatting.WHITE))
+                .append(Component.literal("/" + maxMana)
+                        .withStyle(ChatFormatting.GRAY)), false);
+
         return 1;
     }
-    
+
     private static int setMana(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
         CommandSourceStack source = context.getSource();
-        ServerPlayer player = source.getPlayerOrThrow();
+        ServerPlayer player = source.getPlayerOrException();
         int amount = IntegerArgumentType.getInteger(context, "amount");
-        
+
         ManaManager manaManager = ElementPluginFabric.getInstance().getManaManager();
         PlayerData data = ElementPluginFabric.getInstance().getDataStore().getPlayerData(player.getUUID());
         data.setMana(amount);
         ElementPluginFabric.getInstance().getDataStore().save(data);
-        
+
         source.sendSuccess(() -> Component.literal("Mana set to: ")
-            .withStyle(ChatFormatting.GREEN)
-            .append(Component.literal(String.valueOf(amount))
-                .withStyle(ChatFormatting.WHITE)), false);
-        
+                .withStyle(ChatFormatting.GREEN)
+                .append(Component.literal(String.valueOf(amount))
+                        .withStyle(ChatFormatting.WHITE)), false);
+
         return 1;
     }
 }
-
